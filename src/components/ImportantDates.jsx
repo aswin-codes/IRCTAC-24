14abe340ee78159352003846999c809ac959ebc4
swarna170 <155662259+swarna170@users.noreply.github.com>
2024-08-31 17:19:30 +0530

    Update ImportantDates.jsx

--- src/components/ImportantDates.jsx
+++ src/components/ImportantDates.jsx
@@ -9,34 +9,81 @@ const ImportantDates = () => {
                     <table className='text-sm sm:text-lg w-full'>
                         <tbody>
                             <tr className='border-y-8 border-transparent'>
-                                <td className='w-56 text-right font-bold whitespace-nowrap'><p className='mr-2 sm:mr-10'>3<sup>rd</sup> June 2024</p></td>
-                                <td className='w-8'><div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div></td>
-                                <td className='text-left'><p className='ml-2 sm:ml-10 w-fit'>Call for papers</p></td>
+                                <td className='w-56 text-right font-bold whitespace-nowrap'>
+                                    <p className='mr-2 sm:mr-10'>
+                                        3<sup>rd</sup> June 2024
+                                    </p>
+                                </td>
+                                <td className='w-8'>
+                                    <div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div>
+                                </td>
+                                <td className='text-left'>
+                                    <p className='ml-2 sm:ml-10 w-fit'>Call for Papers</p>
+                                </td>
                             </tr>
                             <tr className='border-y-8 border-transparent'>
-                                <td className='w-56 text-right font-bold whitespace-nowrap'><p className='mr-2 sm:mr-10'>1<sup>st</sup>September 2024</p></td>
-                                <td className='w-8'><div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div></td>
-                                <td className='text-left'><p className='ml-2 sm:ml-10 w-fit'>Paper submission deadline</p></td>
+                                <td className='w-56 text-right font-bold whitespace-nowrap'>
+                                    <p className='mr-2 sm:mr-10'>
+                                        <s className='text-red-500'>1<sup>st</sup> September 2024</s><br />
+                                        8<sup>th</sup> September 2024 (Extended)
+                                    </p>
+                                </td>
+                                <td className='w-8'>
+                                    <div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div>
+                                </td>
+                                <td className='text-left'>
+                                    <p className='ml-2 sm:ml-10 w-fit'>Paper Submission Deadline</p>
+                                </td>
                             </tr>
                             <tr className='border-y-8 border-transparent'>
-                                <td className='w-56 text-right font-bold whitespace-nowrap'><p className='mr-2 sm:mr-10'>30<sup>th</sup> September 2024</p></td>
-                                <td className='w-8'><div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div></td>
-                                <td className='text-left'><p className='ml-2 sm:ml-10 w-fit'>Acceptance notification</p></td>
+                                <td className='w-56 text-right font-bold whitespace-nowrap'>
+                                    <p className='mr-2 sm:mr-10'>
+                                        <s className='text-red-500'>30<sup>th</sup> September 2024</s><br />
+                                        7<sup>th</sup> October 2024 (Extended)
+                                    </p>
+                                </td>
+                                <td className='w-8'>
+                                    <div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div>
+                                </td>
+                                <td className='text-left'>
+                                    <p className='ml-2 sm:ml-10 w-fit'>Acceptance Notification</p>
+                                </td>
                             </tr>
                             <tr className='border-y-8 border-transparent'>
-                                <td className='w-56 text-right font-bold whitespace-nowrap'><p className='mr-2 sm:mr-10'>15<sup>th</sup> October 2024</p></td>
-                                <td className='w-8'><div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div></td>
-                                <td className='text-left'><p className='ml-2 sm:ml-10 w-fit'>Camera Ready Copy submission</p></td>
+                                <td className='w-56 text-right font-bold whitespace-nowrap'>
+                                    <p className='mr-2 sm:mr-10'>
+                                        <s className='text-red-500'>15<sup>th</sup> October 2024</s><br />
+                                        21<sup>st</sup> October 2024 (Extended)
+                                    </p>
+                                </td>
+                                <td className='w-8'>
+                                    <div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div>
+                                </td>
+                                <td className='text-left'>
+                                    <p className='ml-2 sm:ml-10 w-fit'>Camera Ready Copy Submission</p>
+                                </td>
                             </tr>
                             <tr className='border-y-8 border-transparent'>
-                                <td className='w-56 text-right font-bold whitespace-nowrap'><p className='mr-2 sm:mr-10'>5<sup>th</sup> November 2024</p></td>
-                                <td className='w-8'><div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div></td>
-                                <td className='text-left'><p className='ml-2 sm:ml-10 w-fit'>Registration Close</p></td>
+                                <td className='w-56 text-right font-bold whitespace-nowrap'>
+                                    <p className='mr-2 sm:mr-10'>5<sup>th</sup> November 2024</p>
+                                </td>
+                                <td className='w-8'>
+                                    <div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div>
+                                </td>
+                                <td className='text-left'>
+                                    <p className='ml-2 sm:ml-10 w-fit'>Registration Close</p>
+                                </td>
                             </tr>
                             <tr className='border-y-8 border-transparent'>
-                                <td className='w-56 text-right font-bold whitespace-nowrap'><p className='mr-2 sm:mr-10'>14<sup>th</sup> & 15<sup>th</sup> November 2024</p></td>
-                                <td className='w-8'><div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div></td>
-                                <td className='text-left'><p className='ml-2 sm:ml-10 w-fit'>Conference</p></td>
+                                <td className='w-56 text-right font-bold whitespace-nowrap'>
+                                    <p className='mr-2 sm:mr-10'>14<sup>th</sup> & 15<sup>th</sup> November 2024</p>
+                                </td>
+                                <td className='w-8'>
+                                    <div className="w-4 h-4 rounded-full bg-purple-600 mx-auto"></div>
+                                </td>
+                                <td className='text-left'>
+                                    <p className='ml-2 sm:ml-10 w-fit'>Conference</p>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
