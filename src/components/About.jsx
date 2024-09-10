aea18e0e3873f3973bbaf3773b3d5b41713a3ac6
swarna170 <155662259+swarna170@users.noreply.github.com>
2024-09-10 21:29:41 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -164,7 +164,7 @@ const About = () => {
                         <h1 className='text-xl sm:text-2xl font-semibold text-purple-800'>Publishing & Indexing</h1>
                         <hr className='h-[2px] bg-purple-700' />
                         <p className='mt-3'>
-                        <b className='font-bold'>ICRTAC 24 proceedings is being published by Springer's Communications in Computer and Information Science series (CCIS) </b>. All accepted and presented papers will be published by CCIS subject to meeting Springer's scope and quality requirements.<b className='font-bold'> Springer CCIS series is indexed in SCOPUS.</b>
+                        <b className='font-bold'>ICRTAC24 is planned to publish the proceedings  with Springer in their Communications in Computer and Information Science series (final approval pending) </b>. All accepted and presented papers will be published by CCIS subject to meeting Springer's scope and quality requirements.<b className='font-bold'> Springer CCIS series is indexed in SCOPUS.</b>
                         </p>
                         <div className='mt-3 flex lg:flex-row flex-col justify-around items-center my-2 gap-4'>
                             <img src={Springer} alt='Springer Publications' className='object-contain h-10 sm:h-16' />
